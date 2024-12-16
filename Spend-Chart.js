6d1d6b8eb877d169f8f27bdec2f4be4d84413fe2
Chanmonyreach <131956359+Chanmonyreach@users.noreply.github.com>
2024-12-16 20:02:04 +0700

    Update Spend-Chart.js

--- Spend-Chart.js
+++ Spend-Chart.js
@@ -75,7 +75,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     };
 
-    const IP = "localhost";
+    const IP = "https://93ea-203-80-164-104.ngrok-free.app";
 
     let currentChart;
     const userID = localStorage.getItem('userID');
